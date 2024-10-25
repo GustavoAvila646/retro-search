b1bb0f45cf1ee5eac15b89b6bc58686ff275e7a3
Gustavo Avila <105921907+GustavoAvila646@users.noreply.github.com>
2024-10-25 08:22:54 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# retro-search
+## 🎮 Demo
+Clique aqui: [Retro Search - Demo](https://ijrodrigues.github.io/retro-search/)
+
+# Retro Search
+
+Este é o projeto **Retro Search**, criado para a **Imersão de Desenvolvimento** feita pela **Alura** e pelo **Google Gemini**. O objetivo do projeto é criar um buscador de jogos retro, onde os usuários podem pesquisar por jogos, filtrar por plataformas e ser redirecionado para jogar os títulos.
+
+![Print do Site](https://github.com/ijrodrigues/retro-search/blob/master/assets/captura-de-tela.png?raw=true)
+
+## 📚 Descrição
+O **Retro Search** permite que você pesquise por jogos retro clássicos e os jogue diretamente no navegador. Você pode buscar por título ou filtrar por plataforma, como Nintendo, Master System, e PlayStation.
+
+## 🚀 Tecnologias
+Este projeto foi desenvolvido usando apenas:
+- **HTML5**
+- **CSS3**
+- **JavaScript (ES6+)**
+
+## 🛠️ Como Executar Localmente
+
+1. Clone o repositório:
+   git clone https://github.com/ijrodrigues/retro-search.git
+
+2. Navegue até o diretório do projeto:
+   cd retro-search
+
+3. Abra o arquivo index.html
+  Você pode abrir o arquivo index.html diretamente em seu navegador para visualizar o projeto.
+
+## 💡 Como Contribuir
+Se você quiser contribuir com melhorias, novas funcionalidades ou correções de bugs, siga as etapas abaixo:
+
+Faça um fork deste repositório.
+Crie uma nova branch para a sua feature (git checkout -b feature/nova-feature).
+Faça commit das suas alterações (git commit -m 'Adicionar nova feature').
+Faça push para a branch (git push origin feature/nova-feature).
+Abra um Pull Request.
+
+
+## 📜 Licença
+Este projeto está licenciado sob os termos da licença MIT.
+Todos os direitos sobre os jogos, imagens e marcas mencionadas pertencem às suas respectivas empresas.
+Não há intenção de violar qualquer direito autoral ou de propriedade intelectual.
+Este projeto foi desenvolvido apenas para fins educacionais e não possui fins comerciais.
---
+++ mini-game.js
@@ -0,0 +1,157 @@
+let hits = 0;
+let currentAlienIndex = 0;
+let timerInterval = null;
+let startTime = null;
+let timerRunning = false;
+let gameEnded = false; // Variável para controlar o fim do jogo
+
+// Lista de aliens com diferentes imagens e número de hits
+const aliens = [
+
+    { imagePath: 'assets/alien1.gif', hitsToKill: 15, class: 'alien' },
+    { imagePath: 'assets/alien2.gif', hitsToKill: 20, class: 'alien2' },
+    { imagePath: 'assets/alien3.gif', hitsToKill: 30, class: 'alien3' },
+    { imagePath: 'assets/alien4.gif', hitsToKill: 30, class: 'alien4' },
+    { imagePath: 'assets/alien5.gif', hitsToKill: 100, class: 'alien5' },
+];
+
+const collisionSound = document.getElementById('collision-sound');
+const hahahaSound = document.getElementById('hahaha-sound');
+const deathSound = document.getElementById('death-sound');
+const noSound = document.getElementById('no-sound');
+const darthVaderSound = document.getElementById('darth-vader-sound');
+const pacmanSound = document.getElementById('pacman-sound');
+const tartarugaSound = document.getElementById('tartaruga-sound');
+const restartButton = document.getElementById('restartButton');
+const nave = document.getElementById('nave');
+const timerElement = document.getElementById('timer');
+
+// Função para iniciar o cronômetro
+function startTimer() {
+    if (timerRunning || gameEnded) return; // Não reiniciar se o jogo acabou
+
+    startTime = Date.now();
+    timerRunning = true;
+
+    timerInterval = setInterval(() => {
+        const elapsedTime = Date.now() - startTime;
+        const minutes = Math.floor(elapsedTime / 60000).toString().padStart(2, '0');
+        const seconds = Math.floor((elapsedTime % 60000) / 1000).toString().padStart(2, '0');
+        timerElement.textContent = `${minutes}:${seconds}`;
+    }, 1000);
+}
+
+// Função para parar o cronômetro
+function stopTimer() {
+    clearInterval(timerInterval);
+    timerRunning = false;
+}
+
+// Função para criar um novo alien com base no índice atual
+function criarAlien() {
+    const alienData = aliens[currentAlienIndex];
+
+    if (!alienData) {
+        stopTimer(); // Para o cronômetro quando o último alien for derrotado
+        gameEnded = true; // Define o jogo como finalizado
+        restartButton.style.display = 'block'; // Mostra o botão de reiniciar
+        nave.remove();
+        return;
+    }
+
+    const alien = document.createElement('img');
+    alien.src = alienData.imagePath; // Define a imagem do alien
+    alien.alt = 'alien';
+    alien.classList.add(alienData.class);
+    document.body.appendChild(alien);
+
+    hits = 0; // Reseta o contador de hits para o novo alien
+
+    let hitsToKill = alienData.hitsToKill;
+
+
+    if(currentAlienIndex === 1){
+        hahahaSound.play();
+    }
+
+    if(currentAlienIndex === 2){
+        pacmanSound.play();
+    }
+
+    if(currentAlienIndex === 3){
+        tartarugaSound.play();
+    }
+
+    if(currentAlienIndex === 4){
+        darthVaderSound.play();
+    }
+
+    // Função para verificar colisão do tiro com o alien
+    function checkCollision(tiro) {
+        const tiroRect = tiro.getBoundingClientRect();
+        const alienRect = alien.getBoundingClientRect();
+
+        if (tiroRect.top <= alienRect.bottom &&
+            tiroRect.bottom >= alienRect.top &&
+            tiroRect.left >= alienRect.left &&
+            tiroRect.right <= alienRect.right) {
+
+            hits += 1;
+            collisionSound.play();
+            tiro.remove();
+            clearInterval(tiro.checkCollision);
+
+            // Verifica se o alien atingiu o limite de hits
+            if (hits >= hitsToKill) {
+                deathSound.play();
+                alien.remove();
+                if(currentAlienIndex === 4){
+                    noSound.play();
+                }
+                currentAlienIndex += 1;
+                criarAlien(); // Cria o próximo alien
+            }
+        }
+    }
+
+    // Disparar tiros quando o usuário clica
+    document.addEventListener('click', criarTiro);
+
+    function criarTiro() {
+        if (gameEnded) return; // Não dispara tiros se o jogo acabou
+
+        if (!timerRunning) {
+            startTimer(); // Inicia o cronômetro ao disparar o primeiro tiro
+        }
+
+        const nave = document.querySelector('.nave');
+        const tiro = document.createElement('div');
+        tiro.classList.add('tiro');
+        tiro.style.left = `${nave.offsetLeft + nave.offsetWidth / 2 - 15}px`;
+        document.body.appendChild(tiro);
+
+        tiro.checkCollision = setInterval(() => checkCollision(tiro), 50);
+
+        // Remover o tiro depois que ele sai da tela
+        setTimeout(() => {
+            tiro.remove();
+            clearInterval(tiro.checkCollision); // Para de checar a colisão se o tiro sair da tela
+        }, 2000);
+    }
+}
+
+// Função para mover a nave junto com o mouse
+document.addEventListener('mousemove', function (event) {
+    if (gameEnded) return; // Impede movimentação após o fim do jogo
+    const nave = document.querySelector('.nave');
+    const x = event.clientX;
+    nave.style.left = `${x}px`;
+});
+
+// Evento para recarregar a página quando o botão de reiniciar for clicado
+restartButton.addEventListener('click', function () {
+    location.reload();
+});
+
+// Inicializa o primeiro alien
+criarAlien();
